563fc16ec87a8b4835610d8bea999dd059a75495
ANISH SAIKIA <f20231133@pilani.bits-pilani.ac.in>
2026-08-28 07:56:23 +0000

    lab01 task1

--- labs/lab01/task1/FA_Gate.v
+++ labs/lab01/task1/FA_Gate.v
@@ -16,10 +16,13 @@ module FA_Gate(
 );
   wire ps, pc1, pc2;
 
-  xor (ps,  a,   b);
-  and (pc1, a,   b);
-  xor (sum, cin, ps);
-  and (pc2, cin, ps);
-  or  (cout, pc1, pc2);
+  and #(2)(pc2, cin, ps);
+  or  #(3)(cout, pc1, pc2);
+  xor #(2) (ps,  a,   b);
+  and #(4) (pc1, a,   b);
+  xor #(2) (sum, cin, ps);
+ 
+  
+
 
 endmodule
